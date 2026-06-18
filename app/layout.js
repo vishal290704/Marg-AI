d5ea2c70ed0dc13a0c9ccdf78655b66d53b32c43
Vishal290704 <vsjee2002@gmail.com>
2026-06-18 16:42:22 +0530

    Footer Created for landing page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,29 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+                 <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/*Header*/}
+            <main className="min-h-screen">
+              {children}
+            </main>
+            {/*Footer*/}
+            <footer>
+              <div>
+                <p>
+                  Made by Vishal Srivastava.
+                </p>
+              </div>
+            </footer>
+         
+          </ThemeProvider>
+
+        </body>
     </html>
   );
 }
