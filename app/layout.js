d0c9aa6fb5c310e1a6db38c1c4e97be5daefc614
Vishal290704 <vsjee2002@gmail.com>
2026-06-19 22:04:57 +0530

    Wrapping layout.js with ClerkProvider

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
+const inter = Inter({subsets:["latin"]});
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,6 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
@@ -50,5 +44,6 @@ export default function RootLayout({ children }) {
 
         </body>
     </html>
+    </ClerkProvider>
   );
 }
