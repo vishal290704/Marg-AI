36c045d32488b56bb4879fe94fd7093c8f4a7cff
Vishal290704 <vsjee2002@gmail.com>
2026-06-19 22:36:13 +0530

    ThemeProvider removed due to version problem and adding signin and signout button

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <div>
     Hello Vishal
-    <br/>
+
     <Button>Hello</Button>
    </div>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,26 @@
+// "use client";
+import {
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  Show,
+} from "@clerk/nextjs";
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+            <Show when="signed-out">
+        <SignInButton />
+        <SignUpButton>
+          <button>Sign Up</button>
+        </SignUpButton>
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </div>
   )
 }
 
