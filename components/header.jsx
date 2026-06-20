4eb1696c6f3bc376a5593d8beaeb2ca5f00d2143
Vishal290704 <vsjee2002@gmail.com>
2026-06-20 12:43:49 +0530

    Navbar and logo

--- components/header.jsx
+++ components/header.jsx
@@ -5,11 +5,22 @@ import {
   UserButton,
   Show,
 } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <header>
+            <nav>
+                <Link href='/'> 
+                    <Image src="/list.png" width={200} height={60} alt="MargAI logo"
+                    className="h-12 py-1 w-auto object-contain"
+                    />
+                </Link>
+            </nav>
+
+
             <Show when="signed-out">
         <SignInButton />
         <SignUpButton>
@@ -20,7 +31,7 @@ const Header = () => {
       <Show when="signed-in">
         <UserButton />
       </Show>
-    </div>
+    </header>
   )
 }
 
