c60797dd7ace2a01f9e82f8c014f63a6d083b1a1
Vishal290704 <vsjee2002@gmail.com>
2026-06-22 21:07:40 +0530

    inngest packages added and database url added

--- components/header.jsx
+++ components/header.jsx
@@ -23,7 +23,7 @@ import {
 
 const Header = () => {
   return (
-    <header className="fixed top-0 w-full border bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+    <header className="fixed top-0 w-full border bg-background/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         {/* Logo */}
         <Link href="/">
