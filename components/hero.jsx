3a064c9d45b3ba0168f6e329891035d23ad07596
Vishal290704 <vsjee2002@gmail.com>
2026-06-23 22:10:28 +0530

    Hero Section making

---
+++ components/hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HeroSection = () => {
+  return <section className="w-full pt-36 md:pt-48 pb-10">
+    <div>
+        <div>
+            <h1>
+                Your AI Career Coach for
+                <br/>
+                Professional Success
+            </h1>
+            <p>
+                Advance your career with personalized guidance, interview prep, and AI-powered tools for job success.
+            </p>
+        </div>
+    </div>
+  </section>;
+};
+
+export default HeroSection;
