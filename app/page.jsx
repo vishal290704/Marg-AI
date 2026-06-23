df052103fecf2ce395d200f936f63d2df38967b4
Vishal290704 <vsjee2002@gmail.com>
2026-06-23 22:01:54 +0530

    Grid background added in hero section

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <div>
-    {/* <Button>Hello</Button> */}
+    <div className="grid-background"></div>
    </div>
   );
 }
