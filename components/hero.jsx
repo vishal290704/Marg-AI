afd584e33284e5541cabea1f730c6eb47833f535
Vishal290704 <vsjee2002@gmail.com>
2026-06-24 21:45:14 +0530

    Banner in Hero Section

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const HeroSection = () => {
   return <section className="w-full pt-36 md:pt-48 pb-10">
-    <div>
+    <div className="space-y-6 text-center">
         <div>
             <h1>
                 Your AI Career Coach for
@@ -20,7 +21,19 @@ const HeroSection = () => {
                 <Button size="lg" className="px-8">Get Started</Button>
             </Link>
             {/* Can add another link button here */}
-            
+
+        </div>
+        <div>
+            <div>
+                <Image
+                src={"/banner.jpeg"}
+                width={1280}
+                height={720}
+                alt="Banner Marg-AI"
+                className="rounded-lg shadow-2xl border mx-auto"
+                priority
+                />
+            </div>
         </div>
     </div>
   </section>;
