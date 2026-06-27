b5e85a5ab9f1b3a5b65a3d3956c32d9535f44d68
Vishal290704 <vsjee2002@gmail.com>
2026-06-27 21:09:39 +0530

    data files added

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,11 @@ export default function Home() {
     <div className="grid-background"></div>
 
     <HeroSection />
+    <section>
+      <div>
+        <h2>Powerful Features for Your Career Growth</h2>
+      </div>
+    </section>
    </div>
   );
 }
