a2a19d0527385b1bbf3736b7a4177b2a8e4c033e
Vishal290704 <vsjee2002@gmail.com>
2026-07-02 23:03:52 +0530

    Google gemini configuration done

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -2,11 +2,43 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+    model:"gemini-1.5-flash",
+})
 
 
-export const generatedAIInsights = async (industry)=>{}
+export const generatedAIInsights = async (industry)=>{
+    const prompt = `
+          Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
+          {
+            "salaryRanges": [
+              { "role": "string", "min": number, "max": number, "median": number, "location": "string" }
+            ],
+            "growthRate": number,
+            "demandLevel": "High" | "Medium" | "Low",
+            "topSkills": ["skill1", "skill2"],
+            "marketOutlook": "Positive" | "Neutral" | "Negative",
+            "keyTrends": ["trend1", "trend2"],
+            "recommendedSkills": ["skill1", "skill2"]
+          }
+          
+          IMPORTANT: Return ONLY the JSON. No additional text, notes, or markdown formatting.
+          Include at least 5 common roles for salary ranges.
+          Growth rate should be a percentage.
+          Include at least 5 skills and trends.
+        `;
 
-export async function getIndustryInsights(params) {
+        const result = await model.generateContent(prompt)
+        const response = result.response;
+        const text = response.text();
+         const cleanedText = text.replace(/```(?:json)?\n?/g, "").trim();
+         return JSON.parse(cleanedText);
+}
+
+export async function getIndustryInsights() {
         const {userId} = await auth();
         if(!userId) throw new Error("Unauthorized")
     
