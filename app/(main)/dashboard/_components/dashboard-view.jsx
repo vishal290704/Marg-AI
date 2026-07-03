9f4a903cf316c7855a313b19ac15b8a5a743ff41
Vishal290704 <vsjee2002@gmail.com>
2026-07-03 21:48:53 +0530

    salaryData, getmarketOutlookInfo function created in Dashboard-view file

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -1,7 +1,49 @@
 "use client"
-import React from 'react'
+
+import React from "react";
+import {BarChart,Bar,XAxis,YAxis,CartesianGrid,Tooltip,ResponsiveContainer,} from "recharts";
+import {BriefcaseIcon,LineChart,TrendingUp,TredingDown,Brain,} from "lucide-react";
+import { format, formatDistanceToNow } from "date-fns";
+import {Card,CardContent,CardDescription,CardHeader,  CardTitle,} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+
 
 const DashboardView = ({insights}) => {
+
+    const salaryData = insights.salaryranges.map((range)=>({
+        name:range.role,
+        min:range.min / 1000,
+        max:range.max / 1000,
+        median:range.median / 1000,
+    }))
+
+     const getDemandLevelColor = (level) => {
+    switch (level.toLowerCase()) {
+      case "high":
+        return "bg-green-500";
+      case "medium":
+        return "bg-yellow-500";
+      case "low":
+        return "bg-red-500";
+      default:
+        return "bg-gray-500";
+    }
+  };
+
+    const getMarketOutlookInfo = (outlook) => {
+    switch (outlook.toLowerCase()) {
+      case "positive":
+        return { icon: TrendingU, color: "text-green-500" };
+      case "neutral":
+        return { icon: LineChart, color: "text-yellow-500" };
+      case "negative":
+        return { icon: TrendingDown, color: "text-red-500" };
+      default:
+        return { icon: LineChart, color: "text-gray-500" };
+    }
+  };
+
   return (
     <div>
         
