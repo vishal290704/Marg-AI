8a80ab824bf221bf1b04acfd61b0877dad10a8c6
Vishal290704 <vsjee2002@gmail.com>
2026-07-03 22:23:44 +0530

    Market Outlook, Industry Growth, Demand Level cards completed

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -90,6 +90,58 @@ const DashboardView = ({ insights }) => {
             </p>
           </CardContent>
         </Card>
+
+
+
+       
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Industry Growth
+            </CardTitle>
+            <TrendingUp className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {insights.growthRate.toFixed(1)}%
+            </div>
+            <Progress value={insights.growthRate} className="mt-2" />
+          </CardContent>
+        </Card>
+
+
+
+         <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className={"text-sm font-medium"}>Demand Level</CardTitle>
+            <OutlookIcon className={`h-4 w-4 ${OutlookColor}`} />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{insights.demandLevel}</div>
+              <div
+              className={`h-1 w-full rounded-full mt-2 ${getDemandLevelColor(
+                insights.demandLevel
+              )}`}
+            />
+          </CardContent>
+        </Card>
+
+
+
+         <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className={"text-sm font-medium"}>Market Outlook</CardTitle>
+            <OutlookIcon className={`h-4 w-4 ${OutlookColor}`} />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{insights.marketOutlook}</div>
+            <p className="text-xs text-muted-foreground">
+              Next update {nextUpdateDistance}
+            </p>
+          </CardContent>
+        </Card>
+
+
       </div>
     </div>
   );
