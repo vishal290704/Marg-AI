6cc723d3561c768f8e6e72347ad5f9fa4ab86828
Vishal290704 <vsjee2002@gmail.com>
2026-07-04 21:08:28 +0530

    MockInterviewPage UI creating

--- app/(main)/interview/mock/page.jsx
+++ app/(main)/interview/mock/page.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Quiz from "../_components/quiz";
 
 const MockInterviewPage = () => {
   return (
-    <div>MockInterviewPage</div>
+    <div>
+      <div>
+        <Link href={'/interview'}>
+           <Button variant="link" className="gap-2 pl-0">
+            <ArrowLeft className="h-4 w-4" />
+            Back to Interview Preparation
+          </Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
