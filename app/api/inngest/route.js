bde8bc51bc8c33dabad90449f3f3d9bbb1b62609
Vishal290704 <vsjee2002@gmail.com>
2026-07-04 09:57:10 +0530

    Inngest server function creating issue fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,8 @@
-import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
-
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [generateIndustryInsights],
 });
