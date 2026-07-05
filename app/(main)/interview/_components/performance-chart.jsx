a878d8be08620fe6b6d4e92e5f937508a3295839
Vishal290704 <vsjee2002@gmail.com>
2026-07-05 12:17:49 +0530

    Performance-chart(chartData useState and useEffect for setChartData

--- app/(main)/interview/_components/performance-chart.jsx
+++ app/(main)/interview/_components/performance-chart.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
 
-const PerformanceChart = () => {
-  return (
-    <div>PerformanceChart</div>
-  )
-}
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
-export default PerformanceChart
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const PerformanceChart = ({ assessments }) => {
+
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    if (assessments) {
+      const formattedData = assessments.map((assessment) => ({
+        date: format(new Date(assessment.createdAt), "MMM dd"),
+        score: assessment.quizScore,
+      }));
+      setChartData(formattedData);
+    }
+  }, [assessments]);
+
+
+  
+  return <div>PerformanceChart</div>;
+};
+
+export default PerformanceChart;
