6d84a831ddaa2b9bede9f5caf4361354cdb0d2c5
Vishal290704 <vsjee2002@gmail.com>
2026-07-06 23:35:59 +0530

    Resume info entry form created

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -55,3 +55,13 @@ export const entrySchema = z
       path: ["endDate"],
     }
   );
+
+
+  export const resumeSchema = z.object({
+  contactInfo: contactSchema,
+  summary: z.string().min(1, "Professional summary is required"),
+  skills: z.string().min(1, "Skills are required"),
+  experience: z.array(entrySchema),
+  education: z.array(entrySchema),
+  projects: z.array(entrySchema),
+});
