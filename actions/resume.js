0e50ef8701f64c35eefba01883984d08a47125cb
Vishal290704 <vsjee2002@gmail.com>
2026-07-06 18:44:31 +0530

    saveResume function to save resume in database

--- actions/resume.js
+++ actions/resume.js
@@ -1,3 +1,11 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { revalidatePath } from "next/cache";
+
+
 export async function saveResume(content) {
       const { userId } = await auth();
   if (!userId) throw new Error("Unauthorized");
@@ -8,5 +16,24 @@ export async function saveResume(content) {
 
   if (!user) throw new Error("User not found");
 
-  
+    try {
+    const resume = await db.resume.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        content,
+      },
+      create: {
+        userId: user.id,
+        content,
+      },
+    });
+
+    revalidatePath("/resume");
+    return resume;
+  } catch (error) {
+    console.error("Error saving resume:", error);
+    throw new Error("Failed to save resume");
+  }
 }
