f5ca12a32452ea99f2d525aa47987205f93a03f6
Vishal290704 <vsjee2002@gmail.com>
2026-07-06 20:15:21 +0530

    ResumeBuilder page creating

---
+++ app/(main)/resume/_components/resume-builder.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Save } from "lucide-react";
+import React from "react";
+import { useState } from "react";
+
+const ResumeBuilder = ({ initialContent }) => {
+  
+
+  return (
+    <div data-color-mode="light" className="space-y-4">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+        <h1 className="font-bold gradient-title text-5xl md:text-6xl">
+          Resume Builder
+        </h1>
+       <div className="space-x-2">
+          <Button
+            variant="destructive"
+            onClick={handleSubmit(onSubmit)}
+            disabled={isSaving}
+          >
+            {isSaving ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Saving...
+              </>
+            ) : (
+              <>
+                <Save className="h-4 w-4" />
+                Save
+              </>
+            )}
+          </Button>
+          <Button onClick={generatePDF} disabled={isGenerating}>
+            {isGenerating ? (
+              <>
+                <Loader2 className="h-4 w-4 animate-spin" />
+                Generating PDF...
+              </>
+            ) : (
+              <>
+                <Download className="h-4 w-4" />
+                Download PDF
+              </>
+            )}
+          </Button>
+        </div>
+      </div>
+
+        
+
+
+    </div>
+  );
+};
+
+export default ResumeBuilder;
