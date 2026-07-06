ce1551d04cfd3d643daa7dbaf11646c17bf25e6d
Vishal290704 <vsjee2002@gmail.com>
2026-07-06 18:42:42 +0530

    Resume part started

---
+++ actions/resume.js
@@ -0,0 +1,12 @@
+export async function saveResume(content) {
+      const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  
+}
