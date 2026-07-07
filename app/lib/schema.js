b2f333bbdf18c9e593d02f82793f0eb082f60330
Vishal290704 <vsjee2002@gmail.com>
2026-07-07 15:38:58 +0530

    Cover Letter schema added

--- app/lib/schema.js
+++ app/lib/schema.js
@@ -65,3 +65,9 @@ export const entrySchema = z
   education: z.array(entrySchema),
   projects: z.array(entrySchema),
 });
+
+export const coverLetterSchema = z.object({
+  companyName: z.string().min(1, "Company name is required"),
+  jobTitle: z.string().min(1, "Job title is required"),
+  jobDescription: z.string().min(1, "Job description is required"),
+});
