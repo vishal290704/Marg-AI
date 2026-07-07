de9ed33d860c40064258c489d6fda55b68c30ddc
Vishal290704 <vsjee2002@gmail.com>
2026-07-07 10:59:50 +0530

    Check user before resume part

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,7 +1,9 @@
+import { checkUser } from "@/lib/checkUser";
 import { getResume } from "@/actions/resume";
 import ResumeBuilder from "./_components/resume-builder";
 
 export default async function ResumePage() {
+  await checkUser();           // Ensure user exists
   const resume = await getResume();
 
   return (
